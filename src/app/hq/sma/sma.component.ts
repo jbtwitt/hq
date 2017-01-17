d7b5f413e657cd4ca565c2607b8162691fbad322
jb <jb>
2017-01-16 23:04:22 -0500

    add sma trend

--- src/app/hq/sma/sma.component.ts
+++ src/app/hq/sma/sma.component.ts
@@ -13,6 +13,7 @@ export class SmaComponent implements OnInit, OnChanges {
   @Input('days') days: number = 5;
   @Input('segments') segments: number = 5;
   sma: number[];
+  trend: string;
 
   constructor() { }
 
@@ -24,5 +25,14 @@ export class SmaComponent implements OnInit, OnChanges {
 
   getSma() {
     this.sma = average.sma(this.hqTicker, this.days, this.segments, this.dayIndex);
+    // determine trend
+    let upCount = 0, dnCount = 0;
+    let len = this.segments - 1;
+    for (let i=0,j=1; i<len; i++,j++) {
+      if (this.sma[i] > this.sma[j]) upCount ++;
+      else if (this.sma[i] < this.sma[j]) dnCount ++;
+    }
+    if (upCount == len) this.trend = 'Up Trend';
+    else if (dnCount == len) this.trend = 'Dn Trend';
   }
 }
